464387bba3fa0639d80d9b5da85b2ac9c5004345
Antonio Jesús <aca062@inlumine.ual.es>
2023-03-04 13:06:46 +0100

    Prueba driver notificación procesos y hebras

--- AntiHollowing/Prueba.cpp
+++ AntiHollowing/Prueba.cpp
@@ -1,57 +1,11 @@
-#include <ntifs.h>
+#include <Ntifs.h>
+#include <ntddk.h>
+#include <wdm.h>
 
-NTSTATUS BoosterCreateClose(PDEVICE_OBJECT DeviceObject, PIRP Irp);
-NTSTATUS BoosterWrite(PDEVICE_OBJECT DeviceObject, PIRP Irp);
-void BoosterUnload(PDRIVER_OBJECT DriverObject);
-void sCreateProcessNotifyRoutine(HANDLE ppid, HANDLE pid, BOOLEAN create);
-
-extern "C" NTSTATUS
-DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING /*RegistryPath*/) {
-	KdPrint(("Boster: DriverEntry\n"));
-
-	DriverObject->DriverUnload = BoosterUnload;
-
-	DriverObject->MajorFunction[IRP_MJ_CREATE] = BoosterCreateClose;
-	DriverObject->MajorFunction[IRP_MJ_CLOSE] = BoosterCreateClose;
-	DriverObject->MajorFunction[IRP_MJ_WRITE] = BoosterWrite;
-
-	UNICODE_STRING devName = RTL_CONSTANT_STRING(L"\\Device\\Booster");
-
-	PDEVICE_OBJECT DeviceObject;
-	NTSTATUS status = IoCreateDevice(
-		DriverObject,           // our driver object
-		0,                      // no need for extra bytes
-		&devName,               // the device name
-		FILE_DEVICE_UNKNOWN,    // device type
-		0,                      // characteristics flags
-		FALSE,                  // not exclusive
-		&DeviceObject);         // the resulting pointer
-	if (!NT_SUCCESS(status)) {
-		KdPrint(("Failed to create device object (0x%08X)\n", status));
-		return status;
-	}
-
-	UNICODE_STRING symLink = RTL_CONSTANT_STRING(L"\\??\\Booster");
-	status = IoCreateSymbolicLink(&symLink, &devName);
-	if (!NT_SUCCESS(status)) {
-		KdPrint(("Failed to create symbolic link (0x%08X)\n", status));
-		IoDeleteDevice(DeviceObject);   // important!
-		return status;
-	}
-
-	return STATUS_SUCCESS;
-}
-
-void BoosterUnload(_In_ PDRIVER_OBJECT DriverObject) {
-	KdPrint(("Boster: Driver unload\n"));
-
-	UNICODE_STRING symLink = RTL_CONSTANT_STRING(L"\\??\\Booster");
-	// delete symbolic link
-	IoDeleteSymbolicLink(&symLink);
-
-	// delete device object
-	IoDeleteDevice(DriverObject->DeviceObject);
-}
+DRIVER_DISPATCH HandleCustomIOCTL;
+#define IOCTL_SPOTLESS CTL_CODE(FILE_DEVICE_UNKNOWN, 0x2049, METHOD_BUFFERED, FILE_ANY_ACCESS)
+UNICODE_STRING DEVICE_NAME = RTL_CONSTANT_STRING(L"\\Device\\SpotlessDevice");
+UNICODE_STRING DEVICE_SYMBOLIC_NAME = RTL_CONSTANT_STRING(L"\\??\\SpotlessDeviceLink");
 
 void sCreateProcessNotifyRoutine(HANDLE ppid, HANDLE pid, BOOLEAN create)
 {
@@ -66,27 +20,160 @@ void sCreateProcessNotifyRoutine(HANDLE ppid, HANDLE pid, BOOLEAN create)
 		PsLookupProcessByProcessId(pid, &process);
 		SeLocateProcessImageName(process, &processName);
 
-		DbgPrint("%p %wZ\n\t\t%p %wZ", ppid, parentProcessName, pid, processName);
+		DbgPrint("%d %wZ\n\t\t%d %wZ", ppid, parentProcessName, pid, processName);
 	}
 	else
 	{
-		DbgPrint("Process %p lost child %p", ppid, pid);
+		DbgPrint("Process %d lost child %d", ppid, pid);
 	}
 }
 
-NTSTATUS BoosterCreateClose(PDEVICE_OBJECT DeviceObject, PIRP Irp) {
-	UNREFERENCED_PARAMETER(DeviceObject);
+void sCreateProcessNotifyRoutineEx(PEPROCESS process, HANDLE pid, PPS_CREATE_NOTIFY_INFO createInfo)
+{
+	UNREFERENCED_PARAMETER(process);
+	UNREFERENCED_PARAMETER(pid);
 
+	if (createInfo != NULL)
+	{
+		if (wcsstr(createInfo->CommandLine->Buffer, L"notepad") != NULL)
+		{
+			DbgPrint("[!] Access to launch notepad.exe was denied!");
+			createInfo->CreationStatus = STATUS_ACCESS_DENIED;
+		}
+	}
+}
+
+void sLoadImageNotifyRoutine(PUNICODE_STRING imageName, HANDLE pid, PIMAGE_INFO imageInfo)
+{
+	UNREFERENCED_PARAMETER(imageInfo);
+	PEPROCESS process = NULL;
+	PUNICODE_STRING processName = NULL;
+	PsLookupProcessByProcessId(pid, &process);
+	SeLocateProcessImageName(process, &processName);
+
+	DbgPrint("%wZ (%d) loaded %wZ", processName, pid, imageName);
+}
+
+void sCreateThreadNotifyRoutine(HANDLE pid, HANDLE tid, BOOLEAN create)
+{
+	if (create)
+	{
+		DbgPrint("%d created thread %d", pid, tid);
+	}
+	else
+	{
+		DbgPrint("Thread %d of process %d exited", tid, pid);
+	}
+}
+
+void DriverUnload(PDRIVER_OBJECT dob)
+{
+	DbgPrint("Driver unloaded, deleting symbolic links and devices");
+	IoDeleteDevice(dob->DeviceObject);
+	IoDeleteSymbolicLink(&DEVICE_SYMBOLIC_NAME);
+	PsSetCreateProcessNotifyRoutine(sCreateProcessNotifyRoutine, TRUE);
+	PsRemoveLoadImageNotifyRoutine(sLoadImageNotifyRoutine);
+	PsRemoveCreateThreadNotifyRoutine(sCreateThreadNotifyRoutine);
+	PsSetCreateProcessNotifyRoutineEx(sCreateProcessNotifyRoutineEx, TRUE);
+}
+
+NTSTATUS HandleCustomIOCTL(PDEVICE_OBJECT DeviceObject, PIRP Irp)
+{
+	UNREFERENCED_PARAMETER(DeviceObject);
+	PIO_STACK_LOCATION stackLocation = NULL;
+	CHAR* messageFromKernel = "ohai from them kernelz";
+
+	stackLocation = IoGetCurrentIrpStackLocation(Irp);
+
+	if (stackLocation->Parameters.DeviceIoControl.IoControlCode == IOCTL_SPOTLESS)
+	{
+		DbgPrint("IOCTL_SPOTLESS (0x%x) issued", stackLocation->Parameters.DeviceIoControl.IoControlCode);
+		DbgPrint("Input received from userland: %s", (char*)Irp->AssociatedIrp.SystemBuffer);
+	}
+
+	Irp->IoStatus.Information = strlen(messageFromKernel);
 	Irp->IoStatus.Status = STATUS_SUCCESS;
-	Irp->IoStatus.Information = 0;
+
+	DbgPrint("Sending to userland: %s", messageFromKernel);
+	RtlCopyMemory((char*)Irp->AssociatedIrp.SystemBuffer, messageFromKernel, strlen((char*)Irp->AssociatedIrp.SystemBuffer));
+
 	IoCompleteRequest(Irp, IO_NO_INCREMENT);
+
 	return STATUS_SUCCESS;
 }
 
-NTSTATUS BoosterWrite(PDEVICE_OBJECT, PIRP) {
-	auto status = STATUS_SUCCESS;
+NTSTATUS MajorFunctions(PDEVICE_OBJECT DeviceObject, PIRP Irp)
+{
+	UNREFERENCED_PARAMETER(DeviceObject);
 
+	PIO_STACK_LOCATION stackLocation = NULL;
+	stackLocation = IoGetCurrentIrpStackLocation(Irp);
+
+	switch (stackLocation->MajorFunction)
+	{
+	case IRP_MJ_CREATE:
+		DbgPrint("Handle to symbolink link %wZ opened", DEVICE_SYMBOLIC_NAME);
+		break;
+	case IRP_MJ_CLOSE:
+		DbgPrint("Handle to symbolink link %wZ closed", DEVICE_SYMBOLIC_NAME);
+		break;
+	default:
+		break;
+	}
+
+	Irp->IoStatus.Information = 0;
+	Irp->IoStatus.Status = STATUS_SUCCESS;
+	IoCompleteRequest(Irp, IO_NO_INCREMENT);
+
+	return STATUS_SUCCESS;
+}
+
+extern "C" NTSTATUS
+DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath)
+{
+	UNREFERENCED_PARAMETER(DriverObject);
+	UNREFERENCED_PARAMETER(RegistryPath);
+
+	NTSTATUS status = 0;
+
+	// routine that will execute when our driver is unloaded/service is stopped
+	DriverObject->DriverUnload = DriverUnload;
+
+	// routine for handling IO requests from userland
+	DriverObject->MajorFunction[IRP_MJ_DEVICE_CONTROL] = HandleCustomIOCTL;
+
+	// routines that will execute once a handle to our device's symbolik link is opened/closed
+	DriverObject->MajorFunction[IRP_MJ_CREATE] = MajorFunctions;
+	DriverObject->MajorFunction[IRP_MJ_CLOSE] = MajorFunctions;
+
+	DbgPrint("Driver loaded");
+
+	// subscribe to notifications
 	PsSetCreateProcessNotifyRoutine(sCreateProcessNotifyRoutine, FALSE);
+	PsSetLoadImageNotifyRoutine(sLoadImageNotifyRoutine);
+	PsSetCreateThreadNotifyRoutine(sCreateThreadNotifyRoutine);
+	PsSetCreateProcessNotifyRoutineEx(sCreateProcessNotifyRoutineEx, FALSE);
+	DbgPrint("Listeners isntalled..");
 
-	return status;
+	IoCreateDevice(DriverObject, 0, &DEVICE_NAME, FILE_DEVICE_UNKNOWN, FILE_DEVICE_SECURE_OPEN, FALSE, &DriverObject->DeviceObject);
+	if (!NT_SUCCESS(status))
+	{
+		DbgPrint("Could not create device %wZ", DEVICE_NAME);
+	}
+	else
+	{
+		DbgPrint("Device %wZ created", DEVICE_NAME);
+	}
+
+	status = IoCreateSymbolicLink(&DEVICE_SYMBOLIC_NAME, &DEVICE_NAME);
+	if (NT_SUCCESS(status))
+	{
+		DbgPrint("Symbolic link %wZ created", DEVICE_SYMBOLIC_NAME);
+	}
+	else
+	{
+		DbgPrint("Error creating symbolic link %wZ", DEVICE_SYMBOLIC_NAME);
+	}
+
+	return STATUS_SUCCESS;
 }
